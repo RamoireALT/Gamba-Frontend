f607f57843c8febfb2945bdc9f5665ffe7cc9ac4
RamoireALT <ramoirealt@seznam.cz>
2025-06-13 19:57:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('login-btn').addEventListener('click', () => {
+  // Redirect to backend Discord OAuth
+  window.location.href = 'https://gamba-backend.onrender.com/auth/discord';
+});
